edcb167290015458400e87bc9baf8939ece336b3
Harikrishna Gupta <harikrishnag@vconstruct.in>
2026-02-04 14:03:10 +0530

    Question 1 issues resolved-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Header from './Header';
 
-const H1_CONTENT ="Welcome to React !";
+const HEADER_TEXT = "React Fundamentals Training";
 function App() {
   return (
     <>
-      <Header text = {"React Fundamentals Training"}/>
-      <h1>{H1_CONTENT}</h1>
+      <Header text = {HEADER_TEXT}/>
+      <h1>Welcome to React !</h1>
     </>
   )
 }
